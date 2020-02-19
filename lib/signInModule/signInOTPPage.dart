5188a25ffe23ce835e92327827e0565da119dfa8
Amagnum <cse180001001@iiti.ac.in>
2020-02-19 16:14:24 +0530

    changed

--- lib/signInModule/signInOTPPage.dart
+++ lib/signInModule/signInOTPPage.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
-class MySingInOTPPage extends StatefulWidget {
-=======
 class MySignInOTPPage extends StatefulWidget {
->>>>>>> 1a70d3f1d9b5b98795dc80b2948816ab22b7fcdd
   @override
   _MySignInOTPPageState createState() => _MySignInOTPPageState();
 }
