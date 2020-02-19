b5e7b3dbca2f6c01df911a873bc520e761620c79
Shaikh-Ubaid <shaikhubaid769@gmail.com>
2020-02-19 11:12:11 +0530

    Added OTP UI

--- lib/signInModule/signInOTPPage.dart
+++ lib/signInModule/signInOTPPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pin_entry_text_field/pin_entry_text_field.dart';
 
 class MySignInOTPPage extends StatefulWidget {
   @override
@@ -8,8 +9,35 @@ class MySignInOTPPage extends StatefulWidget {
 class _MySignInOTPPageState extends State<MySignInOTPPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("OTP Page"),
+        ),
+        body: Container(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Text("Kindly Enter the OTP received"),
+                PinEntryTextField(
+                  fields: 6,
+                  onSubmit: (String pin) {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text("Pin"),
+                            content: Text('Pin entered is $pin'),
+                          );
+                        }); //end showDialog()
+                  }, // end onSubmit
+                ),
+                RaisedButton(
+                  onPressed: null,
+                  child: Text('Next'),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
