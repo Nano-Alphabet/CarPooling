cdca047bc761acf2a67f4446d8e0a1466ad3dd3b
ruchir-123 <ruchir.mehta2112@gmail.com>
2020-05-29 20:22:24 +0530

    message

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         initialRoute: '/init',
         routes: {
           // '/init': (context) => RefreshPage(),
-          '/init': (context) => RefreshPage(),
+          '/init': (context) => HomePage(),
           '/sign': (context) => SignUp(),
           '/home': (context) => HomePage(),
         },
